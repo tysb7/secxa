ab34a8f3fb7aad9f1f959b0a2b66ede56f13add7
unknown <tysb7>
2019-10-24 14:12:56 +0800

    201910241412

--- src/components/common/footBar.vue
+++ src/components/common/footBar.vue
@@ -1,27 +1,58 @@
 <template>
   <div class="footBar">
-    <el-row class="foot">
+    <div class="foot">
+       <el-row >
       <el-col :span="6">
         <div class="grid-content bg-purple">
-          <div class="footList">x</div>
+          <div class="footList">
+            <p class="footTitle">标准型DV SSL证书</p>
+            <p class="footInfo">COMODO DV PositiveSSL SingleDomain</p>
+            <p class="footInfo">COMODO DV PositiveSSL Multi-Domain</p>
+            <p class="footInfo">COMODO DV PositiveSSL Wildcard SSL</p>
+            <p class="footInfo">TRUSTOCEAN DV SecureSSL IP SSL</p>
+          </div>
         </div>
       </el-col>
       <el-col :span="6" >
          <div class="grid-content bg-purple">
-         <div class="footList">x</div>
+         <div class="footList">
+            <p class="footTitle">企业型OV SSL证书</p>
+            <p class="footInfo">COMODO OV Instant SSL</p>
+            <p class="footInfo">COMODO OV Premium Wildcard SSL</p>
+            <p class="footInfo">COMODO OV UCC SSL</p>
+         </div>
         </div>
       </el-col>
       <el-col :span="6" >
          <div class="grid-content bg-purple">
-          <div class="footList">x</div>
+          <div class="footList">
+             <p class="footTitle">增强型EV SSL证书</p>
+            <p class="footInfo">COMODO EV Positive Multi Domain SSL</p>
+             <p class="footInfo">COMODO EV Positive Single Domain SSL</p>
+              <p class="footInfo">TRUSTOCEAN EV Multi-Domain SSL</p>
+               <p class="footInfo">TRUSTOCEAN EV SSL</p>
+          </div>
         </div>
       </el-col>
       <el-col :span="6" >
          <div class="grid-content bg-purple">
-          <div class="footList">x</div>
+          <div class="footList">
+             <p class="footTitle">联系我们</p>
+            <p class="footInfo">Phone:18091740360</p>
+            <p class="footInfo">Sales:sales@secxa.com</p>
+            <p class="footInfo">Support:support@secxa.com</p>
+            <p class="footInfo">QQ:635308385 / 1159016159</p>
+          </div>
         </div>
       </el-col>
     </el-row>
+    <div style="height:100px;">
+      <el-divider content-position="right"></el-divider>
+      <p class="Copyright">Copyright © 2019 环智中诚SSL. All Rights Reserved. 陕ICP备16011250号-3</p>
+    </div>
+    </div>
+   
+    
   </div>
 </template>
 <script>
@@ -34,7 +65,7 @@ export default {
   background-image: linear-gradient(to right, #051146, #243a9c);
 }
 .foot {
-  height: 120px;
+ 
   max-width: 1200px;
   margin: auto;
 }
@@ -45,8 +76,28 @@ export default {
 
 
 .footList{
-  background-color: #F6F6F6;
-  height: 110px;
-  margin: 10px;
+  color: #ffffff;
+  margin: 80px 10px;
+  text-align: left;
+
+}
+.footTitle{
+  font-size: 18px;
+  line-height: 30px;
+  margin-bottom: 30px;
+  font-weight: 500;
+}
+.footInfo{
+  color: rgba(229,235,255,.56);
+  font-size: 14px;
+  line-height: 25px;
+}
+
+.Copyright{
+  color: #ffffff;
+  text-align: left;
+  font-size: 14px;
+  line-height: 25px;
+  font-weight: 100;
 }
 </style>
